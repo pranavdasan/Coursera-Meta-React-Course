c2eb75f5e6ea25c23aa87381b92e023764d7eb79
Pranav Dasan <pranavdasan8@gmail.com>
2024-06-20 15:56:10 -0700

    Completed "customizing-the-project" for module 1 & deleted unecessary files

--- Module 1/customizing-the-project/src/App.js
+++ Module 1/customizing-the-project/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Intro1 from './components/Intro1.js'
+import Intro2 from './components/Intro2.js'
+import Intro3 from './components/Intro3.js'
+import Nav from './components/Nav.js'
+import Promo from './components/Promo.js'
+import Footer from './components/Footer.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav />,
+      <Promo />,
+      <Intro1 />,
+      <Intro2 />,
+      <Intro3 />,
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
