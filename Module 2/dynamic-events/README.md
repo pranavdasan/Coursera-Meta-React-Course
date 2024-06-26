b50060f7fa26a25b0304fe34f385dc05cb63f7e4
Pranav Dasan <pranavdasan8@gmail.com>
2024-06-25 18:02:25 -0700

    Completed 'dynamic-events' project

---
+++ Module 2/dynamic-events/README.md
@@ -0,0 +1,3 @@
+# Project Description
+In this project, you learn about user events such as onClick, how to make functions for event handling, and how to use this functionality to make websites more user interactive.
+
---
+++ Module 2/dynamic-events/src/App.js
@@ -0,0 +1,28 @@
+function App() {
+  function handleClick() { 
+    let randomNum = Math.floor(Math.random() * 3) + 1;
+    console.log(randomNum);
+    let userInput = prompt('type a number');
+
+    // Check if userInput is null (cancel button) or empty string
+    if (userInput === null || userInput === "") {
+      alert('You cancelled or did not enter a number.');
+      return;
+    }
+
+    if (userInput == randomNum) {
+      alert(`Nice, you guessed it right! The number was ${randomNum}!`);
+    } else {
+      alert(`Nope, you guessed wrong!\n\nComputer's number: ${randomNum}\nYour guess: ${userInput}`);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Task: Add a button and handle a click event</h1>
+      <button onClick={handleClick}>Guess the number between 1 and 3</button>
+    </div>
+  );
+}
+
+export default App;
